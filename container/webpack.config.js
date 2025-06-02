21fbf62afaea756ffc15d81a807133506fe2a3b7
Liam-Ronan-dev <liamronan16@gmail.com>
2025-06-02 14:19:12 +0100

    container & products apps setup with module fed

---
+++ container/webpack.config.js
@@ -0,0 +1,23 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+const ModuleFederationPlugin = require('webpack/lib/container/ModuleFederationPlugin')
+
+module.exports = {
+    mode: 'development',
+    devServer: {
+        port: 8080,
+    },
+    plugins: [
+        // Name only required when we are creating a remote 
+        // Add in for convention
+        new ModuleFederationPlugin({
+            name: 'container',
+            // products in URL - related to 'name' property in products webpack config
+            remotes: {
+                products: 'products@http://localhost:8081/remoteEntry.js'
+            }
+        }),
+        new HtmlWebpackPlugin({
+            template: './public/index.html',
+        })
+    ]
+}
---
+++ products/webpack.config.js
@@ -0,0 +1,33 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const ModuleFederationPlugin = require('webpack/lib/container/ModuleFederationPlugin')
+
+module.exports = {
+    mode: 'development',
+    devServer: {
+        port: 8081,
+    },
+    plugins: [
+        new ModuleFederationPlugin({
+            /**
+             * This string must be identical to the remote URL 
+             * In container webpack config 
+             */
+            name: 'products',
+            // Always use remoteEntry.js
+            filename: 'remoteEntry.js',
+            /**
+             * Choosing what to export from products.
+             * ProductsIndex matches the import statement in src/bootstrap.js
+             * in the container app
+             * Alias because the name in products might be only
+             * meaningful in the products context
+             */
+            exposes: {
+                './ProductsIndex': './src/index'
+            }
+        }),
+        new HtmlWebpackPlugin({
+            template: './public/index.html'
+        }),
+    ]
+};
